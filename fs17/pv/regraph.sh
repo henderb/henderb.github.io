34f188f376992ef0b20a18482605084a9f8c8a7b
Brian Henderson <theinitialsarebh@gmail.com>
2017-05-13 20:04:58 +0000

    Liquid Fertilizer is not required

--- fs17/pv/regraph.sh
+++ fs17/pv/regraph.sh
@@ -2,6 +2,6 @@
 
 dot -Tpng goods.gv > goods.png
 
-cat goods.gv | sed '/Water/d' | sed '/Coal/d' > goods-reduced.gv
+cat goods.gv | sed '/Water/d' | sed '/Coal/d' | sed '/Liquid_Fertilizer/d' > goods-reduced.gv
 dot -Tpng goods-reduced.gv > goods-reduced.png
 
